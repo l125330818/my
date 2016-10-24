4e84e0f8e059ba29e6a81accafdd8cc574f682c7
372350964@qq.com <372350964@qq.com>
2016-10-24 11:00:52 +0800

    test

---
+++ server.js
@@ -0,0 +1,33 @@
+/**
+ * Created by luojie on 2016/10/12 10:17.
+ */
+//var http = require("http");
+//http.createServer(function(request,response){
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
+//    response.end('Hello World!\n');
+//}).listen(8888);
+//var add =  require ("./src/js/commonjstest/add.jsx");
+//add.add();
+////sum.sum();
+//a = 1;
+//console.log(global.a);
+//console.log(__filename);
+//console.log(process);
+//console.log('Server running at http://127.0.0.1:8888/');
+
+
+
+//var webpack = require('webpack');
+//var WebpackDevServer = require('webpack-dev-server');
+//var config = require('./webpack.config');
+//
+//new WebpackDevServer(webpack(config), {
+//    publicPath: config.output.publicPath,
+//    hot: true,
+//    historyApiFallback: true
+//}).listen(3000, 'localhost', function (err, result) {
+//    if (err) {
+//        return console.log(err);
+//    }
+//    console.log('Listening at http://localhost:3000/')
+//});
